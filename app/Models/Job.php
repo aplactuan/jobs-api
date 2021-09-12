7e97e24cbc6e274546ad1c8004470d2ed7cd7b75
Adrian Lactuan <aplactuan@gmail.com>
2021-09-12 23:26:04 +0800

    Add tags to a job

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
     use HasFactory;
 
-    public function tags()
+    public function tags() : BelongsToMany
     {
         return $this->belongsToMany(Tag::class);
     }
