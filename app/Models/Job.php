fc44f54a821ab0278d36c4f53b8cca68c66ed52f
Adrian Lactuan <aplactuan@gmail.com>
2021-09-12 23:11:29 +0800

    Create job and add a user

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -14,7 +15,7 @@ class Job extends Model
         return $this->belongsToMany(Tag::class);
     }
 
-    public function user()
+    public function user() : BelongsTo
     {
         return $this->belongsTo(User::class);
     }
